826ba06a6c66747b562c2814d81eb7b7dffc6d22
Rainmin Chan <rainminchan@gmail.com>
2019-07-08 16:47:39 +0800

    test switching activity by arouter

--- app/src/main/java/com/rainmin/laputa/MainActivity.java
+++ app/src/main/java/com/rainmin/laputa/MainActivity.java
@@ -2,14 +2,19 @@ package com.rainmin.laputa;
 
 import android.os.Bundle;
 
+import com.alibaba.android.arouter.facade.Postcard;
+import com.alibaba.android.arouter.facade.callback.NavigationCallback;
 import com.alibaba.android.arouter.launcher.ARouter;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.annotation.NonNull;
 
+import android.util.Log;
 import android.view.MenuItem;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private TextView mTextMessage;
@@ -25,7 +30,6 @@ public class MainActivity extends AppCompatActivity {
                     return true;
                 case R.id.navigation_dashboard:
                     mTextMessage.setText(R.string.title_dashboard);
-                    ARouter.getInstance().build("/main/main_activity").navigation();
                     return true;
                 case R.id.navigation_notifications:
                     mTextMessage.setText(R.string.title_notifications);
@@ -42,6 +46,30 @@ public class MainActivity extends AppCompatActivity {
         BottomNavigationView navView = findViewById(R.id.nav_view);
         mTextMessage = findViewById(R.id.message);
         navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-    }
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(v ->{
+            Toast.makeText(MainActivity.this, "you clicked button", Toast.LENGTH_SHORT).show();
+            ARouter.getInstance().build("/main/main_activity").navigation(MainActivity.this, new NavigationCallback() {
+                @Override
+                public void onFound(Postcard postcard) {
+                    Log.d("rainmin", "onFound");
+                }
 
+                @Override
+                public void onLost(Postcard postcard) {
+                    Log.d("rainmin", "onLost");
+                }
+
+                @Override
+                public void onArrival(Postcard postcard) {
+                    Log.d("rainmin", "onArrival");
+                }
+
+                @Override
+                public void onInterrupt(Postcard postcard) {
+
+                }
+            });
+        });
+    }
 }
